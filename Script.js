c059b06c6630b918b0c7530bdd1c3ff4a23741f8
Amina do Couto <aminadocouto@outlook.com>
2024-07-04 10:47:32 -0300

    estilo

--- Script.js
+++ Script.js
@@ -53,3 +53,13 @@ function recarregarTarefas(){
 recarregarTarefas()
 
 button.addEventListener('click', addNovaTarefa)
+
+document.addEventListener("DOMContentLoaded", function() {
+    var dataAtual = new Date();
+    var dia = String(dataAtual.getDate()).padStart(2, '0');
+    var mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+    var ano = dataAtual.getFullYear();
+    
+    var dataFormatada = dia + '/' + mes + '/' + ano;
+    document.getElementById('dataAtual').innerText = dataFormatada;
+});
